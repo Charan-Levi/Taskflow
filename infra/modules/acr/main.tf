f4160f7c6d93be0a011aeb40bbf20271dbf12bc1
Charan-Levi <charanriddle@gmail.com>
2026-09-01 20:16:30 +0530

    AcrPull permission for SP

--- infra/modules/acr/main.tf
+++ infra/modules/acr/main.tf
@@ -1,3 +1,4 @@
+data "azurerm_client_config" "current" {}
 resource "azurerm_container_registry" "acr" {
   name                = "${replace(var.name_prefix, "-", "")}acr"
   resource_group_name = var.resource_group_name
@@ -5,4 +6,10 @@ resource "azurerm_container_registry" "acr" {
   sku                 = var.acr_sku
   tags                = var.tags
   admin_enabled       = false
+}
+
+resource "azurerm_role_assignment" "acrpull" {
+  name = "AcrPull"
+  scope = azurerm_container_registry.acr.id
+  principal_id = data.azurerm_client_config.current.object_id
 }
