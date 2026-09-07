c7286ac66afa9a0530ffcfddda8fb7957c4e0685
Charan-Levi <charanriddle@gmail.com>
2026-09-07 16:41:09 +0530

    Add Prod Infra

---
+++ infra/envs/prod/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.3"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.11"
+    }
+  }
+  required_version = ">= 1.15.0, < 2.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
